68e717b69cd5c3a0f49b3a0033e9100eb845810a
Nicole Dow <nikki.dow71@gmail.com>
2019-07-13 01:39:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -26,13 +26,7 @@ def reduce_to_total(array, start_value = 0)
 end
 
 def reduce_to_all_true(source_array)
-  source_array.each do |ele|
-    if ele != true
-      return false
-    else
-      return true
-    end
-  end
+  
 end
 
 
